f1094554a402628f52c56018cf441642eac909be
RegalKing <alexperkoplavec@gmail.com>
2023-07-21 00:51:57 +0200

    User input

--- FirstPrograms/6-UserInput.py
+++ FirstPrograms/6-UserInput.py
@@ -1,19 +1,7 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+height = float(input("How tall are you? "))
 
-# Type casting - converting the data type of a value to some other data type
-
-x = 1   # int
-y = 2.0 # float
-z = "3"   # string
-
-print(y)
-print(int(y)) # this will convert float Y 2.0 to int 2
-
-print (z)
-print(type(z))
-z = int(z)
-print (z)
-print(type(z))
-
-x = float(x)
-print(x)
-print(type(x))
+print("Your name is: "+name)
+print("You will be this old next year: "+str(age+1))
+print("You are "+str(height)+"cm tall")
